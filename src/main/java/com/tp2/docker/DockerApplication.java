deadf3225a450b024e7bef719592876f0e8c2725
José Luiz <jluizbc94@gmail.com>
2025-11-21 04:33:54 -0300

    Projeto Final: Spring Boot + MySQL Custom + Kubernetes

---
+++ src/main/java/com/tp2/docker/DockerApplication.java
@@ -0,0 +1,11 @@
+package com.tp2.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DockerApplication.class, args);
+    }
+}
---
+++ src/test/java/com/tp2/docker/DockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tp2.docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
